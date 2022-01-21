ca687d299b7d006cc7edb8a925a1c886d40716a4
Samiul Haque Sohan <samiulhsohan@gmail.com>
2022-01-21 22:08:34 +0300

    add java lang support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -111,7 +111,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['gradle', 'java'],
+        additionalLanguages: ['java'],
       },
     }),
 };
